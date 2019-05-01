57a1b2dc5201f0291b42c4000bf192e469452001
Yousri <laifayousri@gmail.com>
2019-05-01 23:43:33 +0200

    Création du controller gossips

--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -10,18 +10,35 @@ class GossipsController < ApplicationController
     @city = City.find(@user.city_id)
   end
 
-  def new
+   # Méthode qui permet de créer un nouveau gossip
+   def new
+    @gossip = Gossip.new
   end
 
+  # Méthode qui permet de créer un nouveau gossip avec les params récupéré dans le formulaire de la vue new de gossip
   def create
+    @gossip = Gossip.new(:user_id => params[:user_id], :title => params[:title], :content => params[:content])
+    if @gossip.save
+    redirect_to root_path
+    else
+    render 'new'
+    end
   end
 
   def edit
+    @gossip = Gossip.find(params[:id])
   end
 
   def update
+    @gossip = Gossip.find(params[:id])
+    if @gossip.update(:title => params[:title], :content => params[:content])
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
 
   def destroy
   end
+
 end
