d6d9c7f17adae2793759adb0414c2ab805c82fb1
Yousri <laifayousri@gmail.com>
2019-05-01 19:16:34 +0200

    Création du controller cities

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
  
+  get 'cities/index'
+
+  get 'cities/show'
+
+  get 'cities/new'
+
+  get 'cities/create'
+
+  get 'cities/edit'
+
+  get 'cities/update'
+
+  get 'cities/destroy'
+
   resources :users do 
     resources :gossips 
   end
