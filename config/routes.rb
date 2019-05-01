327b84ef8e9db227b06dcbab58adeb6b525c9cec
Yousri <laifayousri@gmail.com>
2019-05-01 17:00:20 +0200

    Modification des routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,22 @@
 Rails.application.routes.draw do
-  get 'contacts/index'
+ 
+  get 'gossips/index'
 
-  get 'teams/index'
+  get 'gossips/show'
+
+  get 'gossips/new'
+
+  get 'gossips/create'
+
+  get 'gossips/edit'
+
+  get 'gossips/update'
+
+  get 'gossips/destroy'
+
+  get '/team', to: 'teams#index'
+  get '/contact', to: 'contacts#index'
+  get 'welcome/:id', to: 'welcomes#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
