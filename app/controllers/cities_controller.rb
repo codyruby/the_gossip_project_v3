6ce7614d1ed82137c862432db6039be71dbf7476
Yousri <laifayousri@gmail.com>
2019-05-01 22:09:41 +0200

    Modification du controller cities

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,8 +1,13 @@
 class CitiesController < ApplicationController
   def index
+    @cities = City.all
   end
 
   def show
+    @city = City.find(params[:id])
+    @users = User.all
+    @users_by_city_id = @users.where(city_id: params[:id]).ids
+    @gossips = Gossip.all
   end
 
   def new
