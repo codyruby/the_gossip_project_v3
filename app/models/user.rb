bb9627fee0944dccaa7a20ba0125740d779735f7
Yousri <laifayousri@gmail.com>
2019-05-06 20:01:58 +0200

    Modification du model user

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,6 @@ class User < ApplicationRecord
     belongs_to :city
     has_many :gossips
     has_many :comments
+    has_many :likes, dependent: :destroy
+    has_many :gossips, through: :likes
 end
