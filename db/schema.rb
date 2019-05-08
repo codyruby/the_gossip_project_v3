85f4ebf7a83ab539c8c387002a07c2cb4560cdd6
Yousri <laifayousri@gmail.com>
2019-05-08 18:49:05 +0200

    Migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190506160431) do
+ActiveRecord::Schema.define(version: 20190508145139) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,6 +75,7 @@ ActiveRecord::Schema.define(version: 20190506160431) do
     t.datetime "updated_at", null: false
     t.bigint "city_id"
     t.string "password_digest"
+    t.string "remember_digest"
     t.index ["city_id"], name: "index_users_on_city_id"
   end
 
