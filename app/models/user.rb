3611710f0225e174ce89cb4af689a14190e7d8a5
Yousri <laifayousri@gmail.com>
2019-05-08 18:48:01 +0200

    Modification du model User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
+    attr_accessor :remember_token
 
     validates :first_name, :last_name, :city_id, presence: true
     # Validation de l'email par une présence obligatoire et unique avec un format particulier
@@ -12,4 +13,34 @@ class User < ApplicationRecord
     has_many :comments
     has_many :likes, dependent: :destroy
     has_many :gossips, through: :likes
+
+    class << self
+        # Returns the hash digest of the given string.
+        def digest(string)
+          cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                        BCrypt::Engine.cost
+          BCrypt::Password.create(string, cost: cost)
+        end
+    
+        # Returns a random token.
+        def new_token
+          SecureRandom.urlsafe_base64
+        end
+    end
+
+    def remember
+        self.remember_token = User.new_token
+        update_attribute(:remember_digest, User.digest(remember_token))
+    end
+
+     # Returns true if the given token matches the digest.
+    def authenticated?(remember_token)
+        return false if remeber_digest.nil?
+        BCrypt::Password.new(remember_digest).is_password?(remember_token)
+    end
+
+    # Forgets a user.
+    def forget
+        update_attribute(:remember_digest, nil)
+    end
 end
